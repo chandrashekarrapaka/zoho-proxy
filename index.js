e50c95f3574a42c4f0942e3d1ba8cb9fa6af0371
chandrashekarrapaka <rapaka.chandrashekar@gmail.com>
2025-04-11 08:11:26 +0530

    bug fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.post('/zoho-webhook', async (req, res) => {
   try {
     const { name, phone } = req.body;
-    const url = `https://www.zohoapis.in/crm/v7/functions/leadcreationshopify/actions/execute?auth_type=apikey&zapikey=1003.1f099ae6174f2ec18d908f69e7955f76.2c164b6622987d5a4666a8271e2e731d&ln=${encodeURIComponent(name)}}`;
+    const url = `https://www.zohoapis.in/crm/v7/functions/leadcreationshopify/actions/execute?auth_type=apikey&zapikey=1003.1f099ae6174f2ec18d908f69e7955f76.2c164b6622987d5a4666a8271e2e731d&ln=${encodeURIComponent(name)}`;
 
     //const response = await axios.post('https://www.zohoapis.in/crm/v7/functions/createleadincrm/actions/execute?auth_type=apikey&zapikey=1003.07c8503f374f9116af06400203f51266.e5c825a950e4b63ea3c81b59d0dd41cc', req.body);
     const response = await axios.post(url);
